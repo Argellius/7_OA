a56e1efb561c2dfd9b203721b61e539e384bdcf7
Adam Černý <adamcerny0@gmail.com>
2025-02-22 13:36:42 +0100

    dev: zoo: header & footer

---
+++ zoo/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer-links">
+        <a href="about.php">O nás</a>
+        <a href="contact.php">Kontakt</a>
+    </div>
+    <p>2025 ZOO Aplikace. Tvorba WWW aplikací </p>
+</footer>
---
+++ zoo/includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ZOO aplikace</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo"> Zoo Aplikace </div>
+            <ul>
+                <li><a href="index.php">Domů</a></li>
+                <li><a href="list_animals.php">Zvířata</a></li>
+                <li><a href="list_food.php">Jídlo</a></li>
+                <li><a href="list_feeding.php">Krmení</a></li>
+            </ul>
+        </nav>
+    </header>
