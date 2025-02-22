004e32e46a4ccf5cf162005c7fc53e035b6e42c6
Adam Černý <adamcerny0@gmail.com>
2025-02-22 12:25:20 +0100

    dev zvirata sql

--- zoo/sql.sql
+++ zoo/sql.sql
@@ -22,3 +22,18 @@ INSERT into jidlo(nazev, druh) VALUES
 ("Ryby", 1),
 ("Ovoce", 2),
 ("Hmyz", 3);
+
+CREATE TABLE zvirata (
+	id int PRIMARY KEY AUTO_INCREMENT,
+    jmeno varchar(255) NOT NULL,
+    druh_id int NOT NULL,
+    datum_prichodu date not NULL,
+    FOREIGN KEY druh_id REFERENCES druh(id)
+);
+
+INSERT INTO zvirata (jmeno, druh_id, datum_prichodu) VALUES
+("Lev", 1, "2025-02-02"),
+("Slon", 1, "2024-05-12"),
+("Papoušek", 2, "2024-03-12"),
+("Krajta", 3, "2024-06-20"),
+("Sumec", 4, "2024-07-20");
