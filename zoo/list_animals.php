091f293f41d9513f703c835d981d54393ccf3d59
Adam Černý <adamcerny0@gmail.com>
2025-02-22 14:10:28 +0100

    dev: list_animals

---
+++ zoo/list_animals.php
@@ -0,0 +1,41 @@
+<?php
+require "config.php";
+
+$query = "SELECT zvirata.id as id, zvirata.jmeno as jmeno, druhy.nazev as nazev FROM zvirata JOIN druhy ON zvirata.druh_id=druhy.id";
+$statement = $pdo->query($query);
+$animals = $statement->fetchAll();
+?>
+
+
+<?php
+include_once "includes/header.php";
+?>
+
+<main>
+    <h1>Seznam zvířat</h1>
+    <table>
+        <tr>
+            <th>Jméno</th>
+            <th>Druh</th>
+            <th>Akce</th>
+        </tr>
+        <?php foreach ($animals as $animal): ?>
+            <tr>
+                <td><?php echo $animal["jmeno"] //z tabulky ZVIRATA 
+                    ?></td>
+                <td><?php echo $animal["nazev"] //z tabulky DRUHY 
+                    ?></td>
+                <td>
+                    <a href="edit_animal.php?id=<?php echo $animal["id"] ?>">Editovat</a>
+                    <a href="delete_animal.php?id=<?php echo $animal["id"] ?>">Smazat</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+
+    </table>
+</main>
+
+
+<?php
+include_once "includes/footer.php";
+?>
